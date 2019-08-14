d7e29a9fa008e115e84be7546a557b1a92167419
kitambile <odiliakitambile@gmail.com>
2019-08-14 04:47:06 +0000

    d

--- README.py
+++ README.py
@@ -5,5 +5,5 @@ while(x!= 385):
     print("incorrect number")
     print('try again')
     x=int(input("enter account number: "))
-a-int(1000000)
+a=int(1000000)
 print("Your balance is: " + str(a) + " dollars")
