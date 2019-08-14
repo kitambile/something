a956ba8e9b92745168d84cede9d089521c065875
kitambile <odiliakitambile@gmail.com>
2019-08-14 17:45:24 +0000

    e

--- README.py
+++ README.py
@@ -7,3 +7,13 @@ while(x!= 385):
     x=int(input("enter account number: "))
 a=int(1000000)
 print("Your balance is: " + str(a) + " dollars")
+f=input("Do you want to make a deposit or withdfrawal")
+
+while (f=="deposit"):
+    g=int(input("enter amount you want to deposit"))
+    balance=("Your total balance after deposit is: "+ str(a+g))
+    a=a+g
+    print(balance)
+    f=int(inp)
+
+
